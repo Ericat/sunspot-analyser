0a0853cc3ca025c8e56db116067da036d8589a93
Ericat <erica.salvaneschi@hotmail.com>
2014-02-20 19:25:22 +0000

    Calculated scores for each cell

--- grid.rb
+++ grid.rb
@@ -1,16 +1,16 @@
 class Grid
 
-  attr_reader :cells, :grid_size, :number_of_results
+  attr_reader :rows, :grid_size, :number_of_results
 
   def initialize(input)
     data = input.split(' ').map { |x| x.to_i }
     @number_of_results = data[0]
     @grid_size = data[1]
-    @cells = data.drop(2).each_slice(@grid_size).to_a
+    @rows = data.drop(2).each_slice(@grid_size).to_a
   end
 
   def value_at(x, y)
-    valid_range?(x, y) ? @cells[y][x] : 0
+    valid_range?(x, y) ? @rows[y][x] : 0
   end
 
   def score(x, y)
@@ -22,7 +22,12 @@ class Grid
     score += @neighbours.reduce(:+)
   end
   
-  def scores(number_of_results)
+  def scores
+    scores =  []
+    @rows.flatten.each_with_index do |cell, index|
+      scores << score(x(index), y(index)) 
+    end
+    scores
   end
 
   private
@@ -30,6 +35,14 @@ class Grid
   def valid_range?(x, y)
     max_size = @grid_size-1
     x.between?(0, max_size) && y.between?(0, max_size)
+  end
+
+  def y(index)
+    (index - x(index)) / @grid_size
+  end
+
+  def x(index)
+    index % @grid_size
   end
 
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -7,8 +7,8 @@ describe Grid do
     it 'should be initialised with a n of results, a size and an array of numbers' do
       expect(grid.number_of_results).to eq(1)
       expect(grid.grid_size).to eq(3)
-      expect(grid.cells).to be_an_instance_of(Array)
-      expect(grid.cells).to eq([[4, 2, 3], [2, 2, 1], [3, 2, 1]])
+      expect(grid.rows).to be_an_instance_of(Array)
+      expect(grid.rows).to eq([[4, 2, 3], [2, 2, 1], [3, 2, 1]])
     end
   end
   
@@ -25,9 +25,15 @@ describe Grid do
     expect(grid.value_at(3, 3)).to eq(0)
   end
 
-  it 'value at value_at 1,1 should return a score of 20' do
+  it 'should return the right score for each value' do
     expect(grid.score(1, 1)).to eq(20)
     expect(grid.score(0, 0)).to eq(10)
+    expect(grid.score(0, 1)).to eq(15)
+    expect(grid.score(2, 1)).to eq(11)
+  end
+
+  it 'should return an array with each score' do
+    expect(grid.scores).to eq([10, 14, 8, 15, 20, 11, 9, 11, 6])
   end
 
 
